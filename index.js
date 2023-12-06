33cdcbf8a5420c7c6a4850c813d4c8b4d4cc9fd9
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-12-06 13:03:04 +0600

    loaded express, cors, dotenv, mongodb and pinged the server to the mongodb

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require("cors");
+require("dotenv").config(); // to connect to the .env file 
+
+// middleware 
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Doctors Portal server is running");
 });
 
-app.listen(port, () => { 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ubvegtf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const appointmentOptionsCollection = client.db("doctorsPortal").collection("appointmentOptions");
+
+        app.get("/appointmentOptions", async (req, res) => {
+            const query = {};
+            const options = await appointmentOptionsCollection.find(query).toArray();
+            res.send(options);
+        });
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(err => console.error(err));
+
+
+app.listen(port, () => {
     console.log(`Doctors Portal server is running on ${port}`);
 });
